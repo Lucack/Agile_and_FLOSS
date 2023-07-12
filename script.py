407f6979d4cd0e75ff420800804b34b52a9cd00b
Lucack <lucak260@gmail.com>
2023-07-12 20:41:41 -0300

    ACM Total de artigos repetidos: 0

--- script.py
+++ script.py
@@ -1,29 +1,29 @@
 import pandas as pd
 import numpy as np
+import re
 
-antiga = pd.read_csv("Planilha Antiga\SLR_PRINCIPAL - SCOPUS_ORIGINAL.csv")
-# print(antiga.head())
-# print()
-
-
-
-atual = pd.read_csv('Planilhas Finais\current_SLR - Scopus.csv',)
-
+antiga = pd.read_csv("Planilha Antiga\SLR_PRINCIPAL - ACM.csv")
+atual = pd.read_csv("Planilhas Atuais\current_SLR - ACM.csv")
 
 count = 0
-test = "A generalized, enterprise-level systems development process framework for systems analysis and design education"
+
 for art_novo in atual.iloc[1:, 0]:
     for art_antigo in antiga.iloc[1:, 0]:
-        if test == art_antigo:
+
+        # tam = len(art_novo)
+        # i = int(0.05 * tam)
+        # f = int(0.95 * tam)
+        # titulo_menor = art_novo[i:f]
+
+        if art_novo == art_antigo:
             l,c = np.where(atual.values == art_antigo)
-            print("liha",l[0],"  coluna",c[0])
-            # atual.iloc[l[0],c[0]+1] = "ABACAXI"
-            print(atual.iloc[l[0],c[0]+1])
-            print("Novo: ", art_novo)
+            # print("liha",l[0],"  coluna",c[0])
+            atual.iloc[l[0],c[0]+1] = "1"
+            # print(atual.iloc[l[0],c[0]+1])
+            # print("Novo: ", art_novo)
             count = count+1
 
-# Antigo:  A generalized, enterprise-level systems development process framework for systems analysis and design education
-# Novo:  A generalized, enterprise-level systems development process framework for systems analysis and design education
+atual.to_csv("Resultado/current_SLR - ACM Modificado.csv", index=False)
 
-print(count)
-# print(atual.S1.head(35))
+print("Total de artigos repetidos:",count)
+
